f9f2fd13269c8e3256ccb6f4407c88375f1eb5b3
Ivan <spokeme@ya.ru>
2024-01-06 17:52:05 +0300

    Убрал дублирующие import. И прогнал через lint.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 //-----main.js — точка входа. Модуль, который связывает другие модули;
 
-import './data.js';
-import './util.js';
 
-
-import { DESCRIPTION, NAMES, MESSAGE } from './data.js';
 import { generatePhotos } from './util.js';
 
 generatePhotos(10);
